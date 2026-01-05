d317c476dfd59b3c60e54ecb2953a20f855b5ace
K M Marziul Karim <marziulrafi@gmail.com>
2026-01-05 23:24:35 +0600

    Express Server & Application Configuration

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+    try {
+        await prisma.$connect()
+        console.log("Database connected successfully.");
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    }
+    catch (error) {
+        console.error("An error occurred:", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+
+main();
