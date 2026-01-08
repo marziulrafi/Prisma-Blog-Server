abb0bd02c4088863077f6de7b2389646990fabad
K M Marziul Karim <marziulrafi@gmail.com>
2026-01-08 23:40:01 +0600

    Create Post API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express, { Application } from 'express';
-
+import { PostRouter } from './modules/Post/post.router';
 const app: Application = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello!!');
 });  
+
+
+app.use('/posts', PostRouter)
+
 
 export default app;
---
+++ src/modules/Post/post.router.ts
@@ -0,0 +1,13 @@
+import express, { Router } from 'express';
+import { PostController } from './post.controller';
+
+const router = express.Router();
+
+router.post(
+    "/",
+    PostController.createPost
+)
+
+
+
+export const PostRouter: Router = router;
