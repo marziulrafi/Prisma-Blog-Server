6dcaf8beb2fe5f70a2f26819c7ed15936e5f710e
K M Marziul Karim <marziulrafi@gmail.com>
2026-01-09 00:16:21 +0600

    Email Password Authentication

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express, { Application } from 'express';
 import { PostRouter } from './modules/Post/post.router';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
+
 const app: Application = express();
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello!!');
