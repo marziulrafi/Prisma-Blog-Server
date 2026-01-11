eaf4d7319700e96650a5efd20c91c82430788959
K M Marziul Karim <marziulrafi@gmail.com>
2026-01-11 23:09:53 +0600

    Basic Protection

--- src/modules/Post/post.router.ts
+++ src/modules/Post/post.router.ts
@@ -1,10 +1,20 @@
-import express, { Router } from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import { PostController } from './post.controller';
+import { auth as betterAuth } from "../../lib/auth"
 
 const router = express.Router();
 
+const auth = (...roles: any) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const session = await betterAuth.api.getSession({
+            headers: req.headers as any
+        })
+    }
+}
+
 router.post(
     "/",
+    auth("ADMIN", "USER"),
     PostController.createPost
 )
 
