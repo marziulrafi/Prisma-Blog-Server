8791d537e9a4ed2d22bf05b2a6b3e4e622a27b64
K M Marziul Karim <marziulrafi@gmail.com>
2026-01-11 23:41:11 +0600

    Role based Access Control

--- src/modules/Post/post.router.ts
+++ src/modules/Post/post.router.ts
@@ -4,17 +4,66 @@ import { auth as betterAuth } from "../../lib/auth"
 
 const router = express.Router();
 
-const auth = (...roles: any) => {
+export enum UserRole {
+    USER = "USER",
+    ADMIN = "ADMIN"
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
+                id: string,
+                email: string,
+                name: string,
+                role: string,
+                emailVerified: boolean
+            }
+        }
+    }
+}
+
+const auth = (...roles: UserRole[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         const session = await betterAuth.api.getSession({
             headers: req.headers as any
         })
+
+        if (!session) {
+            return res.status(401).json({
+                success: false,
+                message: "You're not authorized!"
+            })
+        }
+        if (!session.user.emailVerified) {
+            return res.status(403).json({
+                success: false,
+                message: "Email verification required. Please verify your email!"
+            })
+        }
+
+        req.user = {
+            id: session.user.id,
+            email: session.user.email,
+            name: session.user.name,
+            role: session.user.role as string,
+            emailVerified: session.user.emailVerified,
+        }
+
+        if (roles.length && !roles.includes(req.user.role as UserRole)) {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden!! You don't have permission to access this."
+            })
+        }
+
+        next()
     }
 }
 
 router.post(
     "/",
-    auth("ADMIN", "USER"),
+    auth(UserRole.USER),
     PostController.createPost
 )
 
