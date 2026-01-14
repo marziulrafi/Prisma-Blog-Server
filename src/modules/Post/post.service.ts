fed1bc375fe0c0c86245e0f727c2fa127bae8aab
K M Marziul Karim <marziulrafi@gmail.com>
2026-01-14 23:44:08 +0600

    Advanced Search using OR and HAS

--- src/modules/Post/post.service.ts
+++ src/modules/Post/post.service.ts
@@ -14,10 +14,25 @@ const createPost = async (data: Omit<Post, "id" | "createdAT" | "updatedAt" | "a
 const getAllPosts = async (payload: { search: string | undefined }) => {
     const allPosts = await prisma.post.findMany({
         where: {
-            title: {
-                contains: payload.search as string,
-                mode: "insensitive"
-            }
+            OR: [
+                {
+                    title: {
+                        contains: payload.search as string,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    content: {
+                        contains: payload.search as string,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    tags: {
+                        has: payload.search as string
+                    }
+                }
+            ]
         }
     })
     return allPosts
