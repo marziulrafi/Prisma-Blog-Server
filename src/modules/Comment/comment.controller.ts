5502ddac148bf8e8edce8e46f6795aa43cdd5e4c
K M Marziul Karim <marziulrafi@gmail.com>
2026-02-11 23:40:37 +0600

    Update Comment

--- src/modules/Comment/comment.controller.ts
+++ src/modules/Comment/comment.controller.ts
@@ -44,8 +44,25 @@ const getCommentsByAuthor = async (req: Request, res: Response) => {
 }
 
 
+const updateComment = async (req: Request, res: Response) => {
+    try {
+        const user = req.user;
+        const { commentId } = req.params;
+        const result = await CommentService.updateComment(commentId as string, req.body, user?.id as string)
+        res.status(200).json(result)
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({
+            error: "Comment update failed!",
+            details: e
+        })
+    }
+}
+
+
 export const CommentController = {
     createComment,
     getCommentById,
-    getCommentsByAuthor
+    getCommentsByAuthor,
+    updateComment
 }
